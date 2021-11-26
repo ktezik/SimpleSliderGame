999b696b4de2f51e2902be784de9405c4e049759
Ivan Grishin <ktezik@yandex.ru>
2021-11-26 16:10:34 +0300

    Initial Commit

---
+++ SimpleSliderGame/SimpleSliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSliderGameApp.swift
+//  SimpleSliderGame
+//
+//  Created by Иван Гришин on 26.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
